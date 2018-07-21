90442406941b4b023fc3dd2f716b80dee057b1ca
Gustavo Paez <paez.gustavo@outlook.com>
2018-07-21 09:53:08 -0500

    Change in Shark

--- shark.py
+++ shark.py
@@ -1,6 +1,6 @@
 class Shark:
     def swim(self):
-        print("The shark is swimming.")
+        print("The shark is learn.")
 
     def be_awesome(self):
         print("The shark is being awesome.")
